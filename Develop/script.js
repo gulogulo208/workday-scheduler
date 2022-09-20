03a4af8fa86847a6c34be416864c49a326c06949
Theodore Elgee <elgee.t@gmail.com>
2022-09-19 23:03:58 -0700

    committing html, css, js, and readme.

---
+++ Develop/script.js
@@ -0,0 +1,43 @@
+
+$(document).ready(function () {
+    // Formatting Moment.js for long datetime
+    let todaysDate = moment().format("MMMM Do YYYY hh:mm a");
+    let displayDate = document.getElementById("today");
+    displayDate.innerHTML = todaysDate;
+    let currentHour = moment().format("HH");
+  
+    // get hour from time block being used and compares to current time
+    $(".time-div").each(function () {
+      var timeDiv = $(this).attr("id").split("-")[1];
+      
+      if (currentHour == timeDiv) {
+        $(this).addClass("present");
+        $(this).children(".description").addClass("white-text");
+      } else if (currentHour < timeDiv) {
+        $(this).removeClass("present");
+        $(this).addClass("future");
+      } else if (currentHour > timeDiv) {
+        $(this).removeClass("future");
+        $(this).addClass("past");
+      }
+    });
+  
+    // saves time and values variables to local storage by taking the parent div id and the child element`s value
+    $(".saveBtn").click(function (event) {
+      event.preventDefault();
+      var value = $(this).siblings(".time-block").val();
+      var time = $(this).parent().attr("id").split("-")[1];
+      localStorage.setItem(time, value);
+    });
+  
+    //grabs items from local storage and sets them in their respective divs using JQuery notation
+    $("#hour-09 .time-block").val(localStorage.getItem("09"));
+    $("#hour-10 .time-block").val(localStorage.getItem("10"));
+    $("#hour-11 .time-block").val(localStorage.getItem("11"));
+    $("#hour-12 .time-block").val(localStorage.getItem("12"));
+    $("#hour-13 .time-block").val(localStorage.getItem("13"));
+    $("#hour-14 .time-block").val(localStorage.getItem("14"));
+    $("#hour-15 .time-block").val(localStorage.getItem("15"));
+    $("#hour-16 .time-block").val(localStorage.getItem("16"));
+    $("#hour-17 .time-block").val(localStorage.getItem("17"));
+  });
